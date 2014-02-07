8ce773b004c4d9aa5001bd92082ad1165fd33250
Jannis Widderich <janniswidderich@gmail.com>
2014-02-07 14:58:05 +0100

    daemonizes server

--- README.md
+++ README.md
@@ -5,4 +5,11 @@ Socket Policy server for node.js
 
 logs to stdout
 
-sudo npm config --global get socket-policy-server:port 80
+sudo npm config --global set socket-policy-server:host 127.0.0.1
+
+sudo npm config --global set socket-policy-server:port 843
+
+sudo npm config --global set socket-policy-server:timeout 5000
+
+sudo npm config --global set socket-policy-server:log_request_data true
+
--- server.js
+++ server.js
@@ -1,78 +1,88 @@
-var net = require('net');
-var fs = require('fs');
-var path = require('path');
-var config = require('./config');
+var startStopDaemon = require('start-stop-daemon');
+startStopDaemon({max: 1}, function() {
+  var net = require('net');
+  var fs = require('fs');
+  var path = require('path');
+  var config = require('./config');
 
-var policy = fs.readFileSync(path.join(__dirname, 'socket_policy.xml'));
+  log("config.host: " + config.host);
+  log("config.port: " + config.port);
+  log("config.timeout: " + config.timeout);
+  log("config.log_request_data: " + config.log_request_data);
+  log("process.getuid(): " + process.getuid());
 
-var command = '<policy-file-request/>';
-var commandLength = command.length;
+  var policy = fs.readFileSync(path.join(__dirname, 'socket_policy.xml'));
 
-var connectionCount = 0;
+  var command = '<policy-file-request/>';
+  var commandLength = command.length;
 
-var server = net.createServer(function (socket) {
+  var connectionCount = 0;
 
-    socket.openedAt = new Date();
-    socket.connectionId = ++connectionCount;
-    socket.connectionRemoteAddress = socket.remoteAddress; // remember remote ip
+  var server = net.createServer(function (socket) {
 
-    logForConnection(socket, 'connected');
+      socket.openedAt = new Date();
+      socket.connectionId = ++connectionCount;
+      socket.connectionRemoteAddress = socket.remoteAddress; // remember remote ip
 
-    socket.setTimeout(config.timeout, function () {
-        logForConnection(socket, 'timeout');
-        socket.destroy();
-    });
+      logForConnection(socket, 'connected');
 
-    socket.on('data', function (data) {
-        var message = 'received ';
-        if (config.log_request_data) {
-            message += escape(data.toString()) + ' - ';
-        }
+      socket.setTimeout(config.timeout, function () {
+          logForConnection(socket, 'timeout');
+          socket.destroy();
+      });
 
-        message += data.toString().length + ' bytes - '
-        if (data.toString().slice(0, commandLength) === command) {
-            socket.end(policy);
-            message += 'OK';
-        } else {
-            socket.end();
-            message += 'BAD';
-        }
-        socket.destroy();
-        logForConnection(socket, message);
-    });
+      socket.on('data', function (data) {
+          var message = 'received ';
+          if (config.log_request_data) {
+              message += escape(data.toString()) + ' - ';
+          }
 
-    socket.on('close', function () {
-        logForConnection(socket, 'closed (handled in ' + (new Date() - socket.openedAt) + 'ms)');
-    });
+          message += data.toString().length + ' bytes - '
+          if (data.toString().slice(0, commandLength) === command) {
+              socket.end(policy);
+              message += 'OK';
+          } else {
+              socket.end();
+              message += 'BAD';
+          }
+          socket.destroy();
+          logForConnection(socket, message);
+      });
 
-    socket.on('error', function (e) {
-        logForConnection(socket, 'error receiving data: ' + e);
-        socket.destroy();
-    });
-});
-server.on('error', function (e) {
-    if (e.code == 'EADDRINUSE') {
-        log("server error, address already in use: " + e);
-    } else {
-        log("server error: " + e);
-    }
-});
-server.on('close', function () {
-    log("socket policy server closing after handling " + connectionCount + " connections");
-});
-server.listen(config.port, config.host, function () {
+      socket.on('close', function () {
+          logForConnection(socket, 'closed (handled in ' + (new Date() - socket.openedAt) + 'ms)');
+      });
+
+      socket.on('error', function (e) {
+          logForConnection(socket, 'error receiving data: ' + e);
+          socket.destroy();
+      });
+  });
+  server.on('error', function (e) {
+      if (e.code == 'EADDRINUSE') {
+          log("server error, address already in use: " + e);
+      } else {
+          log("server error: " + e);
+      }
+  });
+  server.on('close', function () {
+      log("socket policy server closing after handling " + connectionCount + " connections");
+  });
+
+  server.listen(config.port, config.host, function () {
     log('socket policy server bound to port ' + config.port + ' on ' + (config.host || 'all IPs'));
+  });
+
+  function logForConnection(socket, message) {
+      log(socket.connectionId + ' - ' + socket.connectionRemoteAddress + ' - ' + message);
+  }
+
+  function log(message) {
+      var t = (new Date()).toISOString();
+      console.log(t + ' - ' + message);
+  }
+
+  function escape(message) {
+      return message.replace(/\n/g, '\\n').replace(/\r/g, '\\r').replace(/\x07\x1b\f\t\v/g, '?');
+  }
 });
-
-function logForConnection(socket, message) {
-    log(socket.connectionId + ' - ' + socket.connectionRemoteAddress + ' - ' + message);
-}
-
-function log(message) {
-    var t = (new Date()).toISOString();
-    console.log(t + ' - ' + message);
-}
-
-function escape(message) {
-    return message.replace(/\n/g, '\\n').replace(/\r/g, '\\r').replace(/\x07\x1b\f\t\v/g, '?');
-}
